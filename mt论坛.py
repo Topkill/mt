f5ad238921a496224f1782d72e1e09cf44301f55
Topkill <56923465+Topkill@users.noreply.github.com>
2023-05-24 11:58:48 +0800

    Update mt论坛.py

--- mt论坛.py
+++ mt论坛.py
@@ -1,128 +1,136 @@
-import requests
 import time
+
+import requests
+import os
 import re
-from lxml import etree
-# def main_handler(event,context):
-#     print('mt')
-username=''
-password=''
-print("------开始执行mt论坛签到------")
-UserAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36'
-session = requests.session()
-headers = {'User-Agent': UserAgent,
-           'referer':'https://bbs.binmt.cc/',
-           }
-url='https://bbs.binmt.cc/member.php?mod=logging&action=login&infloat=yes&handlekey=login&inajax=1&ajaxtarget=fwin_content_login'
-response = session.get(headers=headers, url=url).text
-print(response)
-response= re.sub(r'^<\?xml.*?\?>', '', response)
-html=etree.HTML(response)
-print(html)
-loginhash=html.xpath('//form[@name="login"]/@id')[0].replace('loginform_','')
-formhash=html.xpath('//input[@name="formhash"]/@value')[0]
-cookitime=html.xpath('//input[@name="cookietime"]/@value')[0]
+# print(os.environ)
+username = os.environ.get("USERNAME")
+password = os.environ.get("PASSWORD")
 
-# loginhash = re.findall('loginhash=(.*?)">', response, re.S)[0]
-# formhash = re.findall('formhash" value="(.*?)".*? />', response, re.S)[0]
-# cookietime= re.findall('cookietime" value="(.*?)" .*?/>', response, re.S)[0]
-print(loginhash)
-print(formhash)
-print(cookietime)
-url='https://bbs.binmt.cc/member.php?mod=logging&action=login&loginsubmit=yes&handlekey=login&loginhash='+loginhash+'&inajax=1'
-data={
-    "formhash": formhash,
-    "referer": "https://bbs.binmt.cc/",
-    "loginfield": "username",
-    "username": username,
-    "password": password,
-    "questionid": "0",
-    "answer": "",
-    "cookietime": cookietime,
+
+dic = {
+    'zero':'0',
+    'one':'1',
+    'two':'2',
+    'three':'3',
+    'four':'4',
+    'five':'5',
+    'six':'6',
+    'seven':'7',
+    'eight':'8',
+    'nine':'9',
 }
-response=session.post(url=url,data=data,headers=headers).text
-response= re.sub(r'^<\?xml.*?\?>', '', response)
-# print(response)
-def qiandao():
-    getHashurl = 'https://bbs.binmt.cc/k_misign-sign.html'
-    page_text = session.get(headers=headers, url=getHashurl).text
-    # print(page_text)
-    html= etree.HTML(page_text)
-    formhash=html.xpath('//input[@name="formhash"]/@value')[0]
-    # form = re.findall('formhash" value="(.*?)".*? />', page_text, re.S)[0]
-    # 模拟签到
-    sign_url = 'https://bbs.binmt.cc/plugin.php?id=k_misign:sign&operation=qiandao&formhash='+formhash+'&format=empty&inajax=1&ajaxtarget='
-    page_text2 = session.get(headers=headers, url=sign_url).text
-    # print('11111111111111111111')
-    # print(page_text2)
-    # 验证是否签到成功
-    check = re.findall('<root><(.*?)</root>', page_text2, re.S)
-    if (len(check) != 0):
-        print(f'签到详情：{check}')
-        if ("今日已签" in str(check)):
-            jg = "今天已经签到过了~"
-            print('今天已经签到过了~')
-        elif ("已签到" in str(check)):
-            jg = "今天已经签到过了~"
-            print('今天已经签到过了~')
-        else:
-            jg = "签到成功~"
-            print('签到成功')
-        response = session.get(headers=headers, url=getHashurl).text
-        html = etree.HTML(response)
-        jfjl=html.xpath('//input[@id="lxreward"]/@value')[0]
-        lxqd=html.xpath('//input[@id="lxdays"]/@value')[0]
-        qddj=html.xpath('//input[@id="lxlevel"]/@value')[0]
-        mz=html.xpath('//a[@class="kmuser"]/span/text()')[0]
-        qdpm=html.xpath('//input[@id="qiandaobtnnum"]/@value')[0]
-        zts=html.xpath('//input[@id="lxtdays"]/@value')[0]
-        jib = re.findall('积分奖励</h4>.*?></span>', response, re.S)
-        lxb = re.findall('<h4>连续签到.*?></span>', response, re.S)
-        djb = re.findall('签到等级</h4>.*?></span>', response, re.S)
-        ztsb = re.findall('签到等级</h4>.*?></span>', response, re.S)
 
+
+def catch(func):
+    def wrapper(*args, **kwargs):
         try:
-            # print(f"昵称：{name}\n签到排名：{pm}\n连续签到：{lx}天\n签到等级：LV.{dj}\n获得金币：{jb}\n总天数：{zts}天")
-            message = '''⏰当前时间：{}
-                        MT论坛签到
-                    ####################
-                    账号昵称：{}
-                    签到排名：{}
-                    连续签到：{}天
-                    签到等级：LV.{}
-                    获得金币：{}
-                    总天数：{}天
-                    签到结果：签到成功
-                    ####################
-                    祝您过上美好的一天！'''.format(
-                time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time() + 28800)),
-                mz, qdpm, lxqd, qddj, jfjl, zts)
-            print(message)
-        except:
-            print("获取信息失败")
+            return func(*args, **kwargs)
+        except Exception as e:
+            print(e)
+            return False
+    return wrapper
+
+
+def get_more_data(text):
+    def get_num(i):
+        num = ''
+        for i in re.findall('<span class="(.*?)"></span>',dates[i]):
+            num+=dic[i]
+        return num
+    # with open('a.html','w',encoding='utf-8')as f:
+    #     f.write(text)
+    dic_ = {}
+    dates = re.findall('<b class="pics J_numpic J_animation" style=".*?">(.*?)</b>', text, re.DOTALL)
+    if dates !=[]:
+        dic_['continue'] = get_num(0) # 连续签到天数
+        dic_['class'] = 'LV.'+get_num(1) # 签到等级
+        dic_['award'] = get_num(2) # 积分奖励
+        dic_['all_day'] = get_num(3) # 签到总天数
+    dates = re.findall('<div class="font">(.*?)</div>', text, re.DOTALL)
+    if dates != []:
+        dic_['num'] =dates[0].replace('\r\n','').strip().split('：')[-1] # 签到排名
+    dates = re.findall('<div id="comiis_key".*?<span>(.*?)</span>.*?</div>', text, re.DOTALL)
+    if dates != []:
+        dic_['name'] = dates[0]  # 获取名字
+    return dic_
+
+@catch
+def start():
+    session = requests.Session()
+    push_text = 'mt论坛：'
+    headers = {
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36',
+    }
+    # 获取登陆所需loginhash和formhash
+    getHash_url = 'https://bbs.binmt.cc/member.php?mod=logging&action=login&infloat=yes&handlekey=login&inajax=1&ajaxtarget=fwin_content_login'
+    session.get(headers=headers, url=getHash_url)
+    time.sleep(5)
+    headers['referer']=getHash_url
+    page_text = session.get(headers=headers, url=getHash_url).text
+    print(page_text)
+
+    loginhash_ex = 'loginhash=(.*?)">'
+    formhash_ex = 'formhash" value="(.*?)".*? />'
+
+    loginhash = re.findall(loginhash_ex, page_text, re.S)[0]
+    formhash = re.findall(formhash_ex, page_text, re.S)[0]
+    # print(loginhash, formhash)
+    # 模拟登陆
+    login_url = 'https://bbs.binmt.cc/member.php?mod=logging&action=login&loginsubmit=yes&handlekey=login&loginhash=' + loginhash + '&inajax=1'
+    data = {
+        'formhash': formhash,
+        'referer': 'https://bbs.binmt.cc/index.php',
+        'loginfield': 'username',
+        'username': username,
+        'password': password,
+        'questionid': '0',
+        'answer': '',
+    }
+    page_text1 = session.post(headers=headers, url=login_url, data=data).text
+    # print(page_text1)
+    # 验证是否登陆成功
+    check_ex = 'root'
+    check = re.findall(check_ex, page_text1, re.S)
+    if (len(check) != 0):
+        print('登录成功')
+        push_text = push_text + '登陆成功，'
+        # 获取签到所需的formhash
+        getHash_url1 = 'https://bbs.binmt.cc/k_misign-sign.html'
+        page_text = session.get(headers=headers, url=getHash_url1).text
+        formhash1 = re.findall(formhash_ex, page_text, re.S)[0]
+        # 模拟签到
+        sign_url = 'https://bbs.binmt.cc/plugin.php?id=k_misign:sign&operation=qiandao&format=text&formhash=' + formhash1
+        page_text2 = session.get(headers=headers, url=sign_url).text
+        # 验证是否签到成功
+        check = re.findall(check_ex, page_text2, re.S)
+        if (len(check) != 0):
+            print('签到成功')
+            push_text = push_text + '签到成功'
+            print(push_text)
+            return get_more_data(session.get(headers=headers, url=getHash_url1).text)
+        else:
+            print('签到失败')
+            push_text = push_text + '签到失败'
     else:
-        print('签到失败')
+        print('登陆失败')
+        push_text = push_text + '登陆失败，'
+    print(push_text)
 
-def count():
-    url='https://bbs.binmt.cc/home.php?mod=spacecp&ac=credit'
-    response=session.get(headers=headers,url=url).text
-    html=etree.HTML(response)
-    jb_count=html.xpath('//li[@class="xi1 cl"]/em/following-sibling::text()')[0].strip()
-    print(f'总金币：{jb_count}')
-    jf_count=html.xpath('//li[@class="cl"]/em/following-sibling::text()')[0]
-    print(f'总积分：{jf_count}')
 
-try:
-    p = re.compile(r'errorhandle_login\(\'(.*?)\'')
-    result = p.findall(response)[0]
-    print(result)
-except:
-    try:
-        p = re.compile(r'succeedhandle_login\(.*?, \'(.*?)\',')
-        result = p.findall(response)[0]
-        print(result)
-        if '登录成功' in result:
-            qiandao()
-            count()
-    except:
-        print('mt登录失败')
 
+
+if __name__ == '__main__':
+    from read_file_to_html import read_to_html
+    from mail_to import Mail
+    if not username or not password:
+        print("请设置环境变量 USERNAME PASSWORD")
+        exit(1)
+    data = start()
+    html = read_to_html(data)
+    mail = Mail()
+    if mail.send(html):
+        print("发送成功")
+    else:
+        print(html)
+        print("发送失败")
